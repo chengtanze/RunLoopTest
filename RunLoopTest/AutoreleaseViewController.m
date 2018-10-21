129b975af3b91be565d0d29f6fbb5eceb21df3ab
chengtanze <cheng@chengdeMacBook-Pro.local>
2018-10-21 11:46:12 +0800

    update

--- RunLoopTest/AutoreleaseViewController.m
+++ RunLoopTest/AutoreleaseViewController.m
@@ -8,6 +8,23 @@
 
 #import "AutoreleaseViewController.h"
 
+/*
+ 
+ 1、关于嵌套runloop
+    嵌套的runloop并不是有多个runloop，只是调用栈不同（开启了多个loop循环）或者说嵌套层。如果把runloop理解为一个函数，那么就可以理解为函数r1调用了自身。
+ 
+    因为有一个全局dictory记录线程信息作为key，这个线程对应了唯一的runloop作为value
+ 
+    比如在主线程中又调用了run，这时就是一个嵌套的runloop，他会取代之前的运行循环处理各种事件（有待考证）
+ 
+2、关于autoreleasepool
+ 
+    在进入kCFRunLoopBeforeWaiting的时候，释放并新建
+        AutoreleasePool: _objc_autoreleasePoolPop();
+        _objc_autoreleasePoolPush();
+ */
+
+
 @interface AutoreleaseViewController ()
 
 @end
